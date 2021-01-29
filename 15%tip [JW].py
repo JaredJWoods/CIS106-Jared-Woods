6921a878f0fab8a0f816e7cd4cf4572183c83c91
Woods-CS106 <77637918+Woods-CS106@users.noreply.github.com>
2021-01-28 22:50:12 -0600

    Add files via upload

---
+++ 15%tip [JW].py
@@ -0,0 +1,8 @@
+meal = float(input())
+percent = 15
+tip = meal * percent / 100
+print("Please Tip: " + str(tip))
+
+
+
+# Figure out how to restrict decimals to 2 spaces
---
+++ salestax [JW].py
@@ -0,0 +1,6 @@
+purchase = float(input())
+salestax = purchase * 0.07
+total = salestax + purchase
+print("SUBTOTAL: $" + str(purchase))
+print("TAX: $.0" + str(salestax))
+print("TOTAL: $" + str(total))
